e697f827d48ec69567236251a97769fe03f50a8b
yarramkomala <yarramkomali@gmail.com>
2017-01-06 19:56:02 +0530

    creatingtableindatabase

---
+++ ShoppingCartApplication/src/main/java/com/niit/domain/UserInfo.java
@@ -0,0 +1,56 @@
+package com.niit.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user1")
+public class UserInfo {
+	@Id
+	private String username;
+	private String password;
+	private String fname;
+	private String lname;
+	private String address;
+	private String contactnumber;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContactnumber() {
+		return contactnumber;
+	}
+	public void setContactnumber(String contactnumber) {
+		this.contactnumber = contactnumber;
+	}
+	
+	
+
+}
--- ShoppingCartApplication/src/main/java/com/niit/springprograms/LoginPageController.java
+++ ShoppingCartApplication/src/main/java/com/niit/springprograms/LoginPageController.java
@@ -23,38 +23,15 @@ public class LoginPageController {
 		return "login";
 	}
 	@RequestMapping("/register")
-	public String getRegister() {
-//		dataService.insertRow(userinfo);
-//		  return new ModelAndView("redirect:list");
-		//@ModelAttribute UserInfo userinfo
+	public String getRegister(@ModelAttribute UserInfo userinfo) {
 
+		dataService.insertRow(userinfo);
+		  return new ModelAndView("redirect:list");
+		
 		return "register";
 	}
 	
-	@RequestMapping(value="/display",method=RequestMethod.POST)
 	
-	public ModelAndView getPage(@RequestParam("fname") String a,@RequestParam("lname") String b,@RequestParam("email") String c  ) {
-      System.out.println("login");
-      ModelAndView mv=new ModelAndView("login");
-      mv.addObject("fname",a);
-      mv.addObject("lname", b);
-      mv.addObject("email", c);
-		return mv;
-	}
-//	@RequestMapping("/validate")
-//	public ModelAndView validate(@RequestParam("email") String email,@RequestParam("pwd")String pwd){
-//		System.out.println("validate methode");
-//		ModelAndView mv=new ModelAndView("user");
-//		UserDao userDAO=new UserDao();
-//		if(userDAO.isValidCredentials(email,pwd)==true){
-//			mv.addObject("successmsg","you  logged in successfully");
-////			
-////		}
-////		else{
-////			mv.addObject("errormsg","please try again,");
-////		}
-////		return mv;
-//	}
 	
 }
 
